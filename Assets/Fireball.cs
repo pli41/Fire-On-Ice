344e9378a0d9c4e551534302618f60fe84dea516
Peilin Li <lipeilin@gatech.edu>
2015-04-15 12:25:56 -0400

    Fireball added

---
+++ Assets/Fireball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+
+	public float damage = 20f;
+	public float knockDist = 3f;
+	public float coolDown = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
