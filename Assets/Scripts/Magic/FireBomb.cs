898a6e01965d8ba4b1ece3fdff5e615cf22b2db5
Peilin Li <lipeilin@gatech.edu>
2015-10-04 20:27:11 -0400

    keyboard control implemented; fire bomb ability half way done.

---
+++ Assets/Scripts/Magic/FireBomb.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBomb : Ability, Cooldown, Shootable {
+
+	public float cooldown;
+	public float bombSpeed;
+
+	private FireBomb_Object fireBomb_object;
+
+	// Use this for initialization
+	void Start () {
+		abilityReady = false;
+		SetupCooldown ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CooldownUpdate ();
+	}
+
+	//Cooldown interface
+	public void SetupCooldown(){
+		cdTimer = 0f;
+	}
+	
+	public void ResetCooldown(){
+		cdTimer = 0f;
+		abilityReady = false;
+	}
+
+	public void CooldownUpdate(){
+		if(cdTimer < cooldown){
+			cdTimer += Time.deltaTime;
+		}
+		else{
+
+			abilityReady = true;
+		}
+	}
+
+	public override void Cast(){
+		//Debug.Log ("Casting");
+		if(abilityReady){
+			Shoot();
+			ResetCooldown();
+		}
+		else{
+			Debug.Log("Ability not ready.");
+		}
+	}
+
+	public override void EndCast(){
+		//Debug.Log ("Endcast");
+	}
+	
+	public void Shoot(){
+		Debug.Log ("Shoot");
+		SetupObj ();
+		Instantiate (ability_object);
+	}
+	
+	public override void SetupObj(){
+		Debug.Log (ability_point);
+		fireBomb_object = ability_object.GetComponent<FireBomb_Object> ();
+		Vector3 objVelocity = new Vector3 ();
+		objVelocity = transform.forward * bombSpeed;
+
+
+		//objVelocity.Set ();
+		Debug.Log (objVelocity + " : Velocity");
+		Debug.DrawRay (owner.transform.position, objVelocity);
+
+
+		ability_object.transform.position = ability_point.position;
+		ability_object.transform.rotation = owner.transform.rotation;
+		ability_object.GetComponent<Rigidbody>().velocity = objVelocity;
+	}
+
+
+}
---
+++ Assets/Scripts/Magic/FireBomb_Object.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBomb_Object : MonoBehaviour {
+
+	private bool initialized;
+	private bool attached;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!initialized){
+			Initialize();
+
+		}
+
+		if(attached){
+
+		}
+
+
+
+
+		Debug.Log (GetComponent<Rigidbody> ().velocity);
+	}
+
+	void Explode(){
+
+	}
+
+	void Initialize(){
+		Vector3 objVelocity = transform.forward * 1000f + Vector3.up * 200f;
+		GetComponent<Rigidbody> ().AddForce (objVelocity);
+		initialized = true;
+	}
+
+
+}
