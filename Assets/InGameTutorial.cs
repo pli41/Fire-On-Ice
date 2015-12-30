98a19ce679c2c20f1f5f1b16d2ce8c06ec06d217
Peilin Li <lipeilin@gatech.edu>
2015-12-30 12:37:58 -0500

    added water splash particle effects; fixed material; added in game tutorial

---
+++ Assets/InGameTutorial.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InGameTutorial : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		GetComponent<RectTransform> ().sizeDelta = new Vector2(Screen.width, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
