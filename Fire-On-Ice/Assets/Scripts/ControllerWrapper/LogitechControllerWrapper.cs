3a696a134ae78fc152b672b19e3ec57b86202033
Peilin Li <lipeilin@gatech.edu>
2016-01-07 13:53:53 -0500

    Logitech and keyboard controller wrapper setup

---
+++ Fire-On-Ice/Assets/Scripts/ControllerWrapper/LogitechControllerWrapper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LogitechControllerWrapper : ControllerInputWrapper {
+
+    public override float GetAxis(Axis axis, int joyNum, bool isRaw = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override float GetTrigger(Triggers trigger, int joyNum, bool isRaw = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool GetButton(Buttons button, int joyNum, bool isDown = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool GetButtonUp(Buttons button, int joyNum)
+    {
+        throw new NotImplementedException();
+    }
+}
