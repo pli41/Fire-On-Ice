edd4091133b1a2d24ac03a08bdd719f2966ec7c8
Peilin Li <lipeilin@gatech.edu>
2016-01-13 17:45:03 -0500

    Adjusted pregame arrow positions

---
+++ Fire-On-Ice/Assets/DisableInactiveParticles.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DisableInactiveParticles : MonoBehaviour
+{
+    ParticleSystem.Particle[] unused = new ParticleSystem.Particle[1];
+
+    void Awake()
+    {
+        GetComponent<ParticleSystemRenderer>().enabled = false;
+    }
+
+    void LateUpdate()
+    {
+        GetComponent<ParticleSystemRenderer>().enabled = GetComponent<ParticleSystem>().GetParticles(unused) > 0;
+    }
+}
